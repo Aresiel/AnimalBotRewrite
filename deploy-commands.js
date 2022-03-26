6a010ff7099bf9f31c69605fbd7682ffa4e712da
Aresiel <love.dittlau@gmail.com>
2022-03-26 12:47:09 +0100

    Rewrite in Discord.js with slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('cat').setDescription('A cute little cat!'),
+    new SlashCommandBuilder().setName('dog').setDescription('Man\'s best friend!'),
+    new SlashCommandBuilder().setName('fox').setDescription('Mix between a cat and dog.'),
+    new SlashCommandBuilder().setName('koala').setDescription('Like a huge terrifying bear, but without the huge and terrifying parts.'),
+    new SlashCommandBuilder().setName('horse').setDescription('How else are you supposed to get anywhere? Oh wait, cars.'),
+    new SlashCommandBuilder().setName('wolf').setDescription('Big woof.'),
+    new SlashCommandBuilder().setName('panda').setDescription('All pandas aren\'t black and white.'),
+    new SlashCommandBuilder().setName('otter').setDescription('A cute otter.'),
+    new SlashCommandBuilder().setName('animal-bot-info').setDescription('Contact info, privacy policy, etc.'),
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
---
+++ index.js
@@ -0,0 +1,112 @@
+const { Client, Intents, MessageEmbed } = require('discord.js')
+const config = require('./config.json')
+const fetch = require("node-fetch")
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log(`${client.user.username} is ready!`)
+})
+
+/*
+    TEMPORARY DURING MIGRATION
+    While the bot migrates to slash commands, the old help commands will inform users of the migration.
+ */
+//client.on("messageCreate")
+
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const  { commandName } = interaction;
+
+    if(commandName === "animal-bot-info"){
+
+        const embed = new MessageEmbed()
+            .setColor(3421755)
+            .setThumbnail(client.user.avatarURL())
+            .setTitle("Bot Info")
+            .setDescription("A small bot for seeing pictures of animals.")
+            .addFields(
+                { "name": "How to use", "value": "I have several slash commands available by typing a / and then selecting the one you wish to use in the menu." },
+                { name: "Contact me", "value": "You can contact my creator via discord, his username and tag is Aresiel#0666" },
+                { name: "Privacy policy", "value": "You can read my privacy policy [here](https://github.com/Aresiel/contact-me/blob/main/privacy%20policy%20no%20data.md)." }
+            )
+            .setTimestamp()
+
+        interaction.reply({ embeds: [embed] })
+
+    } else {
+        await interaction.reply({ embeds: [randomAnimalEmbed(commandName)] })
+    }
+})
+
+function imageEmbed(title, url){
+    const embed = new MessageEmbed()
+        .setColor(3421755)
+        .setURL(url)
+        .setImage(url)
+        .setTitle(title)
+    return embed
+}
+
+function randomAnimal(animal){
+    return images[animal][Math.floor(Math.random()*images[animal].length)]
+}
+
+function randomAnimalEmbed(animal){
+    return imageEmbed(animal.charAt(0).toUpperCase() + animal.slice(1), randomAnimal(animal))
+}
+
+let images = {
+    "cat": [],
+    "dog": [],
+    "fox": [],
+    "koala": [],
+    "horse": [],
+    "wolf": [],
+    "panda": [],
+    "otter": []
+}
+
+function updateImages(){
+    // Pixabay animals
+    ["koala", "horse", "wolf", "panda", "otter"].forEach(animal => {
+        fetch(`https://pixabay.com/api/?key=${config.pixabayKey}&q=${animal}&per_page=100&safesearch=true`)
+            .then(res => res.json())
+            .then(json => {
+                json['hits'].forEach(hit => {
+                    images[animal].push(hit['largeImageURL'])
+                })
+            })
+        console.log("Updated " + animal)
+    })
+
+    // Cat
+    fetch('https://api.thecatapi.com/v1/images/search')
+        .then(res => res.json())
+        .then(json => {
+            images['cat'].push(json[0].url)
+        })
+    console.log("Updated cat")
+
+    // Dog
+    fetch('https://api.thedogapi.com/v1/images/search')
+        .then(res => res.json())
+        .then(json => {
+            images['dog'].push(json[0].url)
+        })
+    console.log("Updated dog")
+
+    // Fox
+    fetch('https://randomfox.ca/floof/')
+        .then(res => res.json())
+        .then(json => {
+            images['fox'].push(json.image)
+        })
+    console.log("Updated fox")
+}
+
+setInterval(updateImages, 1000*60*15) // Update every 15 minutes
+updateImages()
+
+client.login(config.token)
